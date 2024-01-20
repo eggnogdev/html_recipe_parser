0e1dbb3ad880a84f7a7b6728c9147906a18774e1
eggnog <codeberg@eggnog.dev>
2024-01-20 17:11:22 +0200

    recursively check for Recipe type schema

--- lib/src/utils/data_fetch.dart
+++ lib/src/utils/data_fetch.dart
@@ -5,7 +5,7 @@ import 'package:html/parser.dart' as html_parser;
 import 'package:html/dom.dart';
 
 class DataFetchService {
-  static Map<String, dynamic> getJsonLd(Document dom) {
+  static Map<String, dynamic>? getJson(Document dom) {
     final scripts = dom.querySelectorAll(
       'script[type="application/ld+json"]',
     );
@@ -14,21 +14,44 @@ class DataFetchService {
       final jsonLd = jsonDecode(script.innerHtml);
 
       if (jsonLd is List) {
-        // we found a list, grab the first one that is a recipe
-        for (final element in jsonLd) {
-          if (element['@type'] == 'Recipe' ||
-              element['@type'].contains('Recipe')) {
-            return element;
-          }
+        for (final json in jsonLd) {
+          final found = _findRecipeJsonLd(json);
+          if (found != null) return found;
         }
-      } else if (jsonLd['@type'] == 'Recipe') {
-        return jsonLd;
+      } else {
+        final found = _findRecipeJsonLd(jsonLd);
+        if (found != null) return found;
       }
     }
 
-    throw Exception(
-      'Failed to find Recipe type JSON-LD',
-    );
+    return null;
+  }
+
+  /// This will recursively check json for a section that has: "@type": "Recipe"
+  static Map<String, dynamic>? _findRecipeJsonLd(Map<String, dynamic> json) {
+    if (json['@type'] == 'Recipe') {
+      return json;
+    } else if (json['@type'] is List) {
+      if ((json['@type'] as List).contains('Recipe')) return json;
+    } else {
+      for (final key in json.keys) {
+        print(json[key]);
+        if (json[key] is List) {
+          // go through every list of JSONs
+          for (final nestedJson in json[key]) {
+            if (nestedJson is Map<String, dynamic>) {
+              final found = _findRecipeJsonLd(nestedJson);
+              if (found != null) return found;
+            }
+          }
+        } else if (json[key] is Map<String, dynamic>) {
+          final found = _findRecipeJsonLd(json[key]);
+          if (found != null) return found;
+        }
+      }
+    }
+
+    return null;
   }
 
   static Future<Document> getDocument(Uri uri) async {
