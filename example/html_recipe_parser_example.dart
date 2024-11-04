0b87fa0b8d49cee6af4bc9753bf768a52ae5c2d9
eggnog <daniel@eggnog.dev>
2024-11-04 20:07:26 +0200

    switch to returning Maps instead of class instances

--- example/html_recipe_parser_example.dart
+++ example/html_recipe_parser_example.dart
@@ -1,9 +1,11 @@
+import 'dart:convert';
+
 import 'package:html_recipe_parser/html_recipe_parser.dart';
 
 void main() async {
-  final parser = RecipeParser();
-  final recipe = await parser.parseUrl(
-    'https://www.allrecipes.com/recipe/75135/swedish-sticky-chocolate-cake-kladdkaka/',
+  final parsed = await HTMLRecipeParser.parseUrl(
+    "https://www.allrecipes.com/recipe/75135/swedish-sticky-chocolate-cake-kladdkaka/",
   );
-  print(recipe);
+
+  print(jsonEncode(parsed));
 }
